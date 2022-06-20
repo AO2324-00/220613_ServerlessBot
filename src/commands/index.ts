beec9b23ebb69d669386328f367e1ce53a86a1ea
AO2324-00 <ao.2324.00@gmail.com>
2022-06-20 19:59:09 +0900

    test

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,26 +2,22 @@ import { ApplicationCommandInteraction, InteractionResponse } from "../../@types
 import { InteractionCallbackType } from "../../@types/types.ts"
 import { ping } from "./ping.ts"
 
-export function onApplicationCommand(interaction:ApplicationCommandInteraction){
+export function onApplicationCommand(interaction:ApplicationCommandInteraction):InteractionResponse {
   const data = interaction.data
   // 実行されたコマンド名
   // コマンドが増えた場合はこれで分岐すれば良い
   const commandName = data.name;
-  let res:InteractionResponse;
-  
-  switch(commandName){
-    case "test":
-      res = ping(interaction);
-      break;
-      
-    default:
-      res = {
-        type:InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content:"This command is unimplemented"
-        }
+
+  const res = {ping}
+
+  const command = res[commandName] || function(){
+    return {
+      type:InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content:"This command is unimplemented"
       }
+    }
   }
   
-  return res;
+  return command(interaction);
 }
